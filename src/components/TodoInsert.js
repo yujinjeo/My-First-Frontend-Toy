d55410ed599fca61ba454b88c64422b3c5636075
sophie_lavender <wjddbwls@ewhain.net>
2024-04-20 21:03:06 +0900

    item delete, update 기능 추가

--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -1,10 +1,27 @@
+import { useState, useCallback } from 'react';
 import {MdAdd} from 'react-icons/md';
 import './scss/TodoInsert.scss';
 
-const TodoInsert = () => {
+const TodoInsert = ({onInsert}) => {
+    const [value, setValue] = useState('');
+
+    const onChange =useCallback(e => {
+        setValue(e.target.value);
+    },[]);
+
+    const onSubmit = useCallback(e=>{
+        onInsert(value);
+        setValue(''); //value 값 초기화 
+        e.preventDefault();
+    }, [onInsert, value]);
+
     return (
-        <form className='TodoInsert'>
-            <input placeholder='할 일을 입력하세요'/>
+        <form className='TodoInsert' onSubmit={onSubmit}>
+            <input 
+                placeholder='할 일을 입력하세요'
+                value={value}
+                onChange={onChange}
+            />
             <button type='submit'>
                 <MdAdd/>
             </button>
